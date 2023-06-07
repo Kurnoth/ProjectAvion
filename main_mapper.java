533201a7decbb259c4b032ca6d3cf93d21750510
Kurnoth <arnaud.kohler@bbox.fr>
2023-06-07 09:11:47 -0100

    RM files

--- main_mapper.java
+++
@@ -1,8 +0,0 @@
-package ProjectAvion;
-public class main_mapper {
-	public static void main(String[] args) {
-		Pos positionRadar = new Pos(48.8333, 2.6167);
-		Map r = new Map(positionRadar);
-	}
-
-}
